1ca3f876ddf625f96e6b657c4af5f5f66d583358
Rits0631 <94890637+Ritesh0604@users.noreply.github.com>
2024-05-28 16:19:10 +0530

    Add pagination to beer list

--- src/BeerList.js
+++ src/BeerList.js
@@ -2,12 +2,15 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import ReactPaginate from 'react-paginate';
 import BeerCard from './BeerCard';
 import './BeerList.css';
 
 const BeerList = () => {
     const [beers, setBeers] = useState([]);
     const [search, setSearch] = useState('');
+    const [currentPage, setCurrentPage] = useState(0);
+    const [beersPerPage] = useState(10); // Number of beers per page
 
     useEffect(() => {
         axios.get('https://api.sampleapis.com/beers/ale')
@@ -19,6 +22,15 @@ const BeerList = () => {
         beer.name.toLowerCase().includes(search.toLowerCase())
     );
 
+    const pageCount = Math.ceil(filteredBeers.length / beersPerPage);
+
+    const changePage = ({ selected }) => {
+        setCurrentPage(selected);
+    };
+
+    const offset = currentPage * beersPerPage;
+    const currentBeers = filteredBeers.slice(offset, offset + beersPerPage);
+
     return (
         <div className="beer-list">
             <input
@@ -29,10 +41,21 @@ const BeerList = () => {
                 className="search-input"
             />
             <div className="beer-cards">
-                {filteredBeers.map(beer => (
+                {currentBeers.map(beer => (
                     <BeerCard key={beer.id} beer={beer} />
                 ))}
             </div>
+            <ReactPaginate
+                previousLabel={'Previous'}
+                nextLabel={'Next'}
+                breakLabel={'...'}
+                pageCount={pageCount}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={3}
+                onPageChange={changePage}
+                containerClassName={'pagination'}
+                activeClassName={'active'}
+            />
         </div>
     );
 };
